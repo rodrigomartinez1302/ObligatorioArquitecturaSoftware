308efa8af2223a60c758442e1aaaafef19791aac
Angel Diaz <warydiaz10@gmail.com>
2018-11-11 19:35:27 -0300

    Se agrego la función que contrla el saldo de la tarjeta desde el primero del mes corriente hasta el día corriente

--- EmisorApp/src/Controlador/controladorEmisor.js
+++ EmisorApp/src/Controlador/controladorEmisor.js
@@ -2,29 +2,36 @@ var persistencia= require("../Persistencia/mongoDBConeccion");
 var luhn = require('luhn-alg');
 
 exports.guardarCompra = async (req) => {
-    await controlarSaldoTarjeta(req);
     await controlarValidezTarjeta(req);
-    var idCompra=await persistencia.guardarCompra(req);
+    await controlarSaldoTarjeta(req);
+    var idCompra = await persistencia.guardarCompra(req);
     await persistencia.actualizarSaldoTarjeta(req);
+
     return idCompra;
 }; 
 exports.eliminarCompra = async (req) => {
     let idCompra=await persistencia.eliminarCompra(req);
+
     return idCompra;
 }; 
 controlarValidezTarjeta= (req) => {
     let control= luhn(req.body.tarjeta.toString());
+
     if(control){
         return control;
     }
+
     throw new Error('Tarjeta Inválida');
 }
 controlarSaldoTarjeta=async (req) => {
-    let saldo= await persistencia.consultarSaldoTarjeta(req);
-    let control= saldo - req.body.monto > 0;
+    //let saldo= await persistencia.consultarSaldoTarjeta(req);
+    let acumulado = await persistencia.consultarTotalComprasEnTarjeta(req);
+    let control= acumulado - req.body.monto > 0;
+
     if(control){
         return control;
     }
+
     throw new Error('Saldo insuficiente');
 }
 actualizarSaldoTarjeta=async (req) => {
